5ee48787cf88dba512e4e63e15c25212636df36b
Harry Albert <65182701+harryalbert@users.noreply.github.com>
2023-08-11 13:45:02 +0000

    fixed dropdown scroll issue,  checkbox bug, all employees selection error, view more button bug

--- ramp-fe-challenge/src/components/InputCheckbox/index.tsx
+++ ramp-fe-challenge/src/components/InputCheckbox/index.tsx
@@ -1,10 +1,14 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
-
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
@@ -12,15 +16,19 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
-      />
-      <input
-        id={inputId}
-        type="checkbox"
-        className="RampInputCheckbox--input"
-        checked={checked}
-        disabled={disabled}
-        onChange={() => onChange(!checked)}
-      />
+      >
+        <input
+          id={inputId}
+          type="checkbox"
+          className="RampInputCheckbox--input"
+          checked={checked}
+          disabled={disabled}
+          onChange={() => {
+            console.log("yes?");
+            onChange(!checked);
+          }}
+        />
+      </label>
     </div>
-  )
-}
+  );
+};
